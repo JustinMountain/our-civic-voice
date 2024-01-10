ce7cbeb285a04154c317d1e3b391f61d4f207a45
justin <mountainj@gmail.com>
2024-01-10 20:51:13 +0000

    Small correction to properly connect to appropriate DB

--- sql/101_create_roles.sql
+++ sql/101_create_roles.sql
@@ -1,11 +1,11 @@
 -- Create the 'automations' role
 CREATE ROLE admin;
 
+-- Connect to the 'connections' database
+\c connections;
+
 -- Grant SELECT, INSERT, UPDATE, DELETE privileges on tableA to the 'automations' role
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE connections.ontario_mpp TO admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ontario_mpp TO admin;
 
 -- Grant SELECT, INSERT, UPDATE, DELETE privileges on tableB to the 'automations' role
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE connections.ontario_mpp_offices TO admin;
-
-
-
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ontario_mpp_offices TO admin;
