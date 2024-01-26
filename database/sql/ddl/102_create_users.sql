276692d32fe927e7955d7f680bd3409599de10f2
justin <mountainj@gmail.com>
2024-01-26 21:31:10 +0000

    Minor architecture adjustments

--- database/sql/ddl/102_create_users.sql
+++ database/sql/ddl/102_create_users.sql
@@ -1,8 +1,8 @@
--- Create the 'automations' user
 CREATE USER automations WITH PASSWORD 'password';
+CREATE USER express WITH PASSWORD 'password';
 
--- Grant the 'automations' role to the 'populate' user
 GRANT admin TO automations;
+GRANT admin TO express;
 
--- Grant the 'automations' user the ability to connect the 'connections' database
 GRANT CONNECT ON DATABASE our_civic_voice TO automations;
+GRANT CONNECT ON DATABASE our_civic_voice TO express;
