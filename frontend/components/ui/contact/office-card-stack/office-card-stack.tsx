cd541dc1de966e7dd1038c216b9173510367cd7e
Justin Mountain <mountainj@gmail.com>
2024-03-04 23:35:25 +0000

    Fixed office cards

--- frontend/components/ui/contact/office-card-stack/office-card-stack.tsx
+++ frontend/components/ui/contact/office-card-stack/office-card-stack.tsx
@@ -5,8 +5,8 @@ import { OfficeInfo } from "@/components/data-layer/interfaces";
 
 export default async function OfficeCardStack(props: {data: OfficeInfo[]}) {
   return (
-    <div className="container flex flex-col items-center py-8 gap-4
-                    md:grid md:grid-cols-2 md:gap-8 md:py-16 md:justify-between md:place-items-center">
+    <div className="container flex flex-col justify-start py-8 gap-4
+                    md:grid md:grid-cols-2 md:gap-8 md:py-16 md:justify-start">
       {props.data.map((officeInfo, index) => (
         <OfficeCard key={officeInfo.officeId || index} data={officeInfo} />
       ))}
--- frontend/components/ui/contact/office-card-stack/office-card.tsx
+++ frontend/components/ui/contact/office-card-stack/office-card.tsx
@@ -16,7 +16,12 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 
-import { EnvelopeClosedIcon, FileTextIcon, BellIcon } from "@radix-ui/react-icons"
+import { EnvelopeClosedIcon, 
+          FileTextIcon, 
+          BellIcon, 
+          RocketIcon, 
+          DesktopIcon, 
+          MagicWandIcon } from "@radix-ui/react-icons"
 
 export default async function OfficeCard(props: {data: OfficeInfo}) {
   const sourceDate = new Date(props.data.timeRetrieved);
@@ -24,15 +29,14 @@ export default async function OfficeCard(props: {data: OfficeInfo}) {
   const month = sourceDate.toLocaleString('default', { month: 'long' });
   const day = sourceDate.getDate().toString().padStart(2, '0');
   const formattedDate = `${year} ${month} ${day}`;
+  const addressSplit = props.data.officeAddress.split('\n');
 
   return (
-    <div className="grow w-11/12
-                    ">
-
+    <div className="grow w-11/12">
       <Card>
         <CardHeader>
           <CardTitle>{props.data.officeType}</CardTitle>
-          <CardDescription>{props.data.officeEmail}</CardDescription>
+          {/* <CardDescription>{props.data.officeEmail}</CardDescription> */}
         </CardHeader>
         <CardContent className="grid gap-1">
           <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
@@ -41,86 +45,112 @@ export default async function OfficeCard(props: {data: OfficeInfo}) {
               <EnvelopeClosedIcon className="mt-px h-5 w-5" />
             </div>
 
+            {/* Mailing Address */}
             <div className="space-y-1">
+              
               <p className="text-sm text-muted-foreground">Mailing Address</p>
               <p className="font-medium ">
-              {props.data.officeAddress.split('\n').map((line, index, array) => (
-                <React.Fragment key={index}>
-                  {line}
-                  {/* Add <br /> after each line except the last one */}
-                  {index < array.length - 1 && <br />}
-                </React.Fragment>
-              ))}
-              <br />
+
+              {props.data.officeTitle.length > 0 ? (
+                <>
+                  {props.data.officeTitle}
+                  <br />
+                </>              
+              ) : (
+                null
+              )}
+              {addressSplit.length > 0 && addressSplit[0].length > 0? (
+                addressSplit.map((line, index, array) => (
+                  <React.Fragment key={index}>
+                    {line}
+                    {/* Add <br /> after each line except the last one */}
+                    {index < array.length && <br />}
+                  </React.Fragment>
+                ))
+              ) : (
+                null
+              )}    
                 {props.data.officeCity}, {props.data.officeProvinceTerritory}<br />
                 {props.data.officePostalCode}
               </p>
             </div>
           </div>
-          <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+
+          {props.data.officeNote.length > 0 ? (
+            <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+              <div className="pl-8 space-y-1">
+                <p className="font-medium ">
+                  {props.data.officeNote}
+                </p>
+              </div>
+            </div>
+          ) : (
+            null
+          )}
+
+          {/* Phone Number */}
+          {props.data.officeTelephone.length > 0 ? (
+            <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+              <div>
+                <br />
+                <BellIcon className="mt-px h-5 w-5" />
+              </div>
+
+              <div className="space-y-1">
+                <p className="text-sm text-muted-foreground">Phone</p>
+                <p className="font-medium ">
+                  {props.data.officeTelephone}
+                </p>
+              </div>
+            </div>
+          ) : (
+            null
+          )}
+
+          {/* Fax Number */}
+          {props.data.officeFax.length > 0 ? (
+            <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+              <div>
+                <br />
+                <FileTextIcon className="mt-px h-5 w-5" />
+              </div>
+
+              <div className="space-y-1">
+                <p className="text-sm text-muted-foreground">Fax</p>
+                <p className="font-medium ">
+                  {props.data.officeFax}
+                </p>
+              </div>
+            </div>
+          ) : (
+            null
+          )}
+
+          {/* Email */}
+          {props.data.officeEmail.length > 0 ? (
+            <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+              <div>
+                <br />
+                <DesktopIcon className="mt-px h-5 w-5" />
+              </div>
+
+              <div className="space-y-1">
+                <p className="text-sm text-muted-foreground">Email</p>
+                <p className="font-medium ">
+                  {props.data.officeEmail}
+                </p>
+              </div>
+            </div>
+          ) : (
+            null
+          )}
+
+          {/* Toll Free */}
+          {props.data.officeTollFree.length > 0 ? (
+            <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
             <div>
               <br />
-              <BellIcon className="mt-px h-5 w-5" />
-            </div>
-
-            <div className="space-y-1">
-              <p className="text-sm text-muted-foreground">Phone</p>
-              <p className="font-medium ">
-                {props.data.officeTelephone}
-              </p>
-            </div>
-          </div>
-          <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
-            <div>
-              <br />
-              <FileTextIcon className="mt-px h-5 w-5" />
-            </div>
-
-            <div className="space-y-1">
-              <p className="text-sm text-muted-foreground">Fax</p>
-              <p className="font-medium ">
-                {props.data.officeFax}
-              </p>
-            </div>
-          </div>
-
-
-
-          <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
-            <div>
-              <br />
-              <FileTextIcon className="mt-px h-5 w-5" />
-            </div>
-
-            <div className="space-y-1">
-              <p className="text-sm text-muted-foreground">Office Title</p>
-              <p className="font-medium ">
-                {props.data.officeTitle}
-              </p>
-            </div>
-          </div>
-
-
-          <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
-            <div>
-              <br />
-              <FileTextIcon className="mt-px h-5 w-5" />
-            </div>
-
-            <div className="space-y-1">
-              <p className="text-sm text-muted-foreground">Office Note</p>
-              <p className="font-medium ">
-                {props.data.officeNote}
-              </p>
-            </div>
-          </div>
-
-
-
-          <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
-            <div>
-              <br />
-              <FileTextIcon className="mt-px h-5 w-5" />
+              <RocketIcon className="mt-px h-5 w-5" />
             </div>
 
             <div className="space-y-1">
@@ -130,25 +160,30 @@ export default async function OfficeCard(props: {data: OfficeInfo}) {
               </p>
             </div>
           </div>
+          ) : (
+            null
+          )}
 
+          {/* TTY */}
+          {props.data.officeTty.length > 0 ? (
+            <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+              <div>
+                <br />
+                <MagicWandIcon className="mt-px h-5 w-5" />
+              </div>
 
-
-          <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
-            <div>
-              <br />
-              <FileTextIcon className="mt-px h-5 w-5" />
+              <div className="space-y-1">
+                <p className="text-sm text-muted-foreground">Office TTY</p>
+                <p className="font-medium ">
+                  {props.data.officeTty}
+                </p>
+              </div>
             </div>
+          ) : (
+            null
+          )}
 
-            <div className="space-y-1">
-              <p className="text-sm text-muted-foreground">Office TTY</p>
-              <p className="font-medium ">
-                {props.data.officeTty}
-              </p>
-            </div>
-          </div>
-
-
-
+          {/* Source */}
           <div className="text-right">
             <Popover>
               <PopoverTrigger>
@@ -156,9 +191,11 @@ export default async function OfficeCard(props: {data: OfficeInfo}) {
                   Source
                 </p>
               </PopoverTrigger>
-              <PopoverContent>
-                {props.data.sourceUrl}<br />
-                Updated: {formattedDate}
+              <PopoverContent className="w-fit text-sm">
+                Updated: {formattedDate}<br />
+                <div className="text-right">
+                  <a href={props.data.sourceUrl} className="underline">Link</a>
+                </div>
               </PopoverContent>
             </Popover>
           </div>
