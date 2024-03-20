c447ae9c1ccb1ad840edcfdaa6830a914350feb1
Justin Mountain <mountainj@gmail.com>
2024-03-20 22:35:13 +0000

    Concern dropdown fixes

--- frontend/components/ui/hero/main-form.tsx
+++ frontend/components/ui/hero/main-form.tsx
@@ -1,5 +1,6 @@
 "use client"
- 
+import * as React from "react"
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -28,7 +29,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
-import { Input } from "@/components/ui/input"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 const concerns = [
   { label: "Climate Change and Environmental Protection", level: "all", key: 1},
@@ -97,6 +98,9 @@ export default function MainForm() {
     console.log(values)
   }
 
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-muted p-8 rounded-xl mx-4 
@@ -107,7 +111,7 @@ export default function MainForm() {
           render={({ field }) => (
             <FormItem className="flex flex-col">
               <FormLabel>What is your concern?</FormLabel>
-              <Popover>
+              <Popover open={open} onOpenChange={setOpen}>
                 <PopoverTrigger asChild>
                   <FormControl>
                     <Button
@@ -127,86 +131,85 @@ export default function MainForm() {
                     </Button>
                   </FormControl>
                 </PopoverTrigger>
-                <PopoverContent className="w-72 p-0">
+                <PopoverContent className="w-full p-0
+                                            md:min-w-96">
                   <Command>
                     <CommandInput
                       placeholder="Search concerns..."
                       className="h-9"
                     />
                     <CommandEmpty>Nothing found.</CommandEmpty>
-                    <CommandGroup>
-                      {concerns.map((concern) => (
-                        <CommandItem
-                          value={concern.label}
-                          key={concern.key}
-                          onSelect={() => {
-                            form.setValue("concern", concern.label)
+                    <ScrollArea className="h-64">
+                      <CommandGroup>
+                        {concerns.map((concern) => (
+                          <CommandItem
+                            value={concern.label}
+                            key={concern.key}
+                            onSelect={() => {
+                              form.setValue("concern", concern.label);
+                              setOpen(false);
 
-                            let level: string;
-                            let description: string;
-                            let nextQuestion: string;
+                              let level: string;
+                              let description: string;
+                              let nextQuestion: string;
 
-                            switch (concern.level) {
-                              case 'fed':
-                                level = 'Federal';
-                                description = concern.label + " is a " + level + " issue."
-                                nextQuestion = "Select the Federal constituency you live in:";
-                                break;
-                              case 'provterr':
-                                level = 'Provincial/Territorial';
-                                description = concern.label + " is a " + level + " issue."
-                                nextQuestion = "Select the Provincial/Territorial constituency you live in:";
-                                break;
-                              case 'mun':
-                                level = 'Municipal';
-                                description = concern.label + " is a " + level + " issue, which unfortunately aren't currently supported.\n\nSearch for your municipal representatives or select another concern."
-                                nextQuestion = "";
-                                break;
-                              case 'all':
-                                level = 'All Levels';
-                                description = concern.label + " can be effected by all levels of government."
-                                nextQuestion = "Choose the Federal or Provincial/Territorial constituency you live in:";
-                                break;
-                              case 'fedprov':
-                                level = 'Federal and Provincial/Territorial';
-                                description = concern.label + " is a " + level + " issue."
-                                nextQuestion = "Select the Federal or Provincial/Territorial constituency you live in:";
-                                break;
-                              case 'provmun':
-                                level = 'Provincial/Territorial and Municipal';
-                                description = concern.label + " is a " + level + " issue.\n\nUnfortunately Municipal issues aren't currently supported, please select your Provincial/Territorial or search for your municipal representatives."
-                                nextQuestion = "Which " + level + "constituency do you live in?";
-                                break;
-                              default:
-                                level = 'Unknown Level';
-                                description = 'Unknown Description';
-                                nextQuestion = 'Unknown Question';
-                            }
+                              switch (concern.level) {
+                                case 'fed':
+                                  level = 'Federal';
+                                  description = concern.label + " is primarily a " + level + " issue."
+                                  nextQuestion = "Select the Federal constituency you live in:";
+                                  break;
+                                case 'provterr':
+                                  level = 'Provincial/Territorial';
+                                  description = concern.label + " is primarily a " + level + " issue."
+                                  nextQuestion = "Select the Provincial/Territorial constituency you live in:";
+                                  break;
+                                case 'mun':
+                                  level = 'Municipal';
+                                  description = concern.label + " is primarily a " + level + " issue, which unfortunately aren't currently supported.\n\nPlease select another concern."
+                                  nextQuestion = "";
+                                  break;
+                                case 'all':
+                                  level = 'All Levels';
+                                  description = concern.label + " can be effected by all levels of government."
+                                  nextQuestion = "Choose the Federal or Provincial/Territorial constituency you live in:";
+                                  break;
+                                case 'fedprov':
+                                  level = 'Federal and Provincial/Territorial';
+                                  description = concern.label + " is primarily a " + level + " issue."
+                                  nextQuestion = "Select the Federal or Provincial/Territorial constituency you live in:";
+                                  break;
+                                case 'provmun':
+                                  level = 'Provincial/Territorial and Municipal';
+                                  description = concern.label + " is primarily a " + level + " issue.\n\nUnfortunately Municipal issues aren't currently supported, please select your Provincial/Territorial or search for your municipal representatives."
+                                  nextQuestion = "Which " + level + "constituency do you live in?";
+                                  break;
+                                default:
+                                  level = 'Unknown Level';
+                                  description = 'Unknown Description';
+                                  nextQuestion = 'Unknown Question';
+                              }
 
-                            // Alters the paragraph text
-                            const levelGuide = document.getElementById("level_guide");
-                            const constituencyInput = document.getElementById("constituency_input");
+                              // Alters the paragraph text
+                              const levelGuide = document.getElementById("level_guide");
+                              const constituencyInput = document.getElementById("constituency_input");
 
-                            levelGuide!.classList.remove("hidden")
-                            levelGuide!.innerText = description;
+                              levelGuide!.classList.remove("hidden")
+                              levelGuide!.innerText = description;
 
-                            constituencyInput!.classList.remove("hidden")
-                            constituencyInput!.getElementsByTagName("label")[0].innerText = nextQuestion;
-
-
-                            // Need to create a map for all of the constituencies which gets dynamically populated based on the level of the concern, the send button then redirects to the appropriate page
-
+                              constituencyInput!.classList.remove("hidden")
+                              constituencyInput!.getElementsByTagName("label")[0].innerText = nextQuestion;
 
 
+                              // Need to create a map for all of the constituencies which gets dynamically populated based on the level of the concern, the send button then redirects to the appropriate page
 
 
+                              // Create a call for the list from the DB above as const
+                              // Create a variable to store the mapped version of this
+                              // On select from the first part of the form, map the variable to the appropriate subset
+                              // This subset is used to populate the second part of the form which is now revealed
 
-
-
-
-
-
-
+                              // Need case for municipal to not show second question
 
 
 
@@ -234,20 +237,30 @@ export default function MainForm() {
 
 
 
-                          }}
-                        >
-                          {concern.label}
-                          <CheckIcon
-                            className={cn(
-                              "ml-auto h-4 w-4",
-                              concern.label === field.value
-                                ? "opacity-100"
-                                : "opacity-0"
-                            )}
-                          />
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
+
+
+
+
+
+
+
+
+                            }}
+                          >
+                            {concern.label}
+                            <CheckIcon
+                              className={cn(
+                                "ml-auto h-4 w-4",
+                                concern.label === field.value
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+
+                    </ScrollArea>
                   </Command>
                 </PopoverContent>
               </Popover>
@@ -256,8 +269,8 @@ export default function MainForm() {
           )}
         />
 
-        <div id="level_guide" className="hidden text-sm">
-          Placeholder
+        <div id="level_guide" className="hidden text-sm pb-4">
+          This is a <span id="" className="font-bold"></span> issue.
         </div>
 
         <div id="constituency_input" className="hidden">
